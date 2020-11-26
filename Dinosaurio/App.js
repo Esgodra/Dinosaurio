b021e4da1a69b6db43652edc69a0a6e78b0f1085
Esgodra <61488533+Esgodra@users.noreply.github.com>
2020-11-26 14:45:56 -0600

    Add files via upload

---
+++ Dinosaurio/App.js
@@ -0,0 +1,14 @@
+//Inicio del ciclo desde 1 hasta 100
+for(var i=1;i <= 100; i++)
+{
+    //Impresion en consola de numero
+    console.log(i);
+}
+//Declaracion de variable 
+var i=1;
+//Inicio del ciclo mientras  sea diferente a 
+while(i<=100)
+{
+    i++;
+    console.log(i);
+}
